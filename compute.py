0f4114649adedf44b2491967fb8bf1f1088b90da
Sycmtic <33992931+Sycmtic@users.noreply.github.com>
2020-08-26 12:12:04 -0700

    add license (#8)
    Co-authored-by: Yongle Lin <yonglel@google.com>

--- compute.py
+++ compute.py
@@ -1,3 +1,19 @@
+"""
+Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 from const import size_str, dts_time_str, duration_time_str, x_str, y_str, ssim_all_str
 from ffmpeg import get_packets_info, get_qp_data, get_ssim_data
 from graph import generate_line_graph
